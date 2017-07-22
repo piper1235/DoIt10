e955b2f6ad64033f7137b78984a6b8b9427614fd
Tony <piper1235@yahoo.com>
2017-07-22 15:05:34 -0700

    First commit.  Have the add button working.

---
+++ DoIt10/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  DoIt10
+//
+//  Created by Tony on 7/22/17.
+//  Copyright © 2017 Anthony Vitrano. All rights reserved.
+//
+
+import Foundation
+
+class Task {
+    var name = ""
+    var important = false
+}
